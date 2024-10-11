1b5505c5bc4596ec4d86a4c85f3dc2709a9adad6
Kazi Omar Faruk <kaziomar144@gmail.com>
2024-10-12 01:13:44 +0600

    Adding Seeder for default user (admin)

---
+++ database/seeders/ItemSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Item;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+class ItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    
+
+    public function run(): void
+    {
+        $faker = Faker::create();
+        for ($i = 0; $i < 20; $i++) {
+            Item::create([
+                'title' => $faker->sentence, 
+                'description' => $faker->paragraph,
+                'user_id' => rand(1, 10),
+                'status' => $faker->randomElement(['pending', 'approved', 'rejected']),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+        
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (User::count() == 0) {
+            User::create([
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'password' => Hash::make("12345678"), 
+                'role' => 'admin',
+                'email_verified_at' => Carbon::now(),
+            ]);
+        }
+    }
+}
